2fbb8d75b46f76c1e9b7057471d0ce844da8bfbc
Norbert <no.boudens@gmail.com>
2022-11-11 11:15:10 +0100

    INT LFSR

--- my_implementation/bit-test.c
+++ my_implementation/bit-test.c
@@ -14,41 +14,69 @@ void printHex(BYTE* text, SIZE length)
     }
 }
 
-
-char* xor_byte(char *A, char *B, int len)
+// Function for left rotation of bytes
+BYTE brotl(BYTE b)
 {
-         for (int i = 0; i < len; i++)     
-                 A[i] ^= B[i];    
-         return A;
-}
- 
-int xor_int(int *A_int, int *B_int, int len)
-{
-    for(int i = 0; i < len; i++)
-        A_int[i] ^= B_int[i];
+    return (b << 1) | (b >> 7);
 }
 
-void from_byte_to_int(int32_t *output, BYTE *input)
+// Function for left rotation of ints
+int8_t irotl(int8_t b)
 {
-    for(int i = 0; i < 24; i+=4)
-        output[i/4] = input[i] ^ (input[i+1] << 8) ^ (input[i+2] << 16) ^ (input[i+3] << 24);
-    
-    output[6] = input[24];
+    return (b << 1) | ((b >> 7) & 0X01010101);
 }
 
-void from_int_to_byte(BYTE *output, int32_t *input)
+void LFSR_in_Int(int32_t* Output, int32_t* Input)
 {
-    int32_t cint;
-    for(int i = 0; i < 24; i+=4)
-    {
-        cint = input[i/4];
-        output[i] = cint;
-        cint >>= 8;
-        output[i+1] = cint;
-        cint >>= 8;
-        output[i+2] = cint;
-        cint >>= 8;
-        output[i+3] = cint;
-    }
-    output[24] = input[7];
+    // complete its inside
+    uint8_t select[25];
+    uint8_t temp[4];
+
+    memcpy(select, Input, 7);
+
+    // compute Int temp;
+    temp[0] = irotl(select[0]) ^ irotl(select[2]) ^ ((select[13] << 1) & 0XFEFEFEFE);
+    temp[1] = irotl(select[1]) ^ irotl(select[3]) ^ ((select[14] << 1) & 0XFEFEFEFE);
+    temp[2] = irotl(select[2]) ^ irotl(select[4]) ^ ((select[15] << 1) & 0XFEFEFEFE);
+    temp[3] = irotl(select[3]) ^ irotl(select[5]) ^ ((select[16] << 1) & 0XFEFEFEFE);
+
+    // one for_loop to shift the content
+    memcpy(Output, Input+1, 20);
+    int8_t new_segment[4] = {select[25], temp[0], temp[1], temp[2]};
+    memcpy(Output+5, new_segment, 4);
+    int32_t new_end = temp[3] ^ 0;
+    //memcpy(Output+6, new_end, 1);
+}
+
+void LFSR_int_Bytes(BYTE* Output, BYTE* Input)
+{
+    BYTE temp = brotl(Input[0]) ^ brotl(Input[2]) ^ (Input[13] << 1);
+
+    for (SIZE i = 1; i <= BLOCK_SIZE - 1; i++)
+        Output[i-1] = Input[i];
+
+    Output[BLOCK_SIZE - 1] = temp;
+}
+
+int main()
+{
+    BYTE Input[25] = "ABCDEFGDIJKLMABCQRSDDDWX"; // fill with some random values
+    BYTE Output[25];
+
+    // call LFSR_in_Bytes 4 times and finally print Output
+    LFSR_int_Bytes(Output, Input);
+    for(int x = 0; x < 3; x++)
+        LFSR_int_Bytes(Output, Output);
+    printf("Result of LFSR_in_Bytes:\n%s\n",Output);
+
+    // convert Input and Output to equivalent Int arrays and
+    // call LFSR_in_Int 1 time with the same Input and print Output
+    int32_t Out[7];
+    LFSR_in_Int(Out, (int32_t *) Input);
+    printf("Result of LFSR_in_Int:\n%s\n", Out);   
+
+    printf("\n");
+    printHex(Output, BLOCK_SIZE);
+    printf("\n");
+    printHex((BYTE *) Out, BLOCK_SIZE);
 }
